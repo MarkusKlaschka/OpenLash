5e783afd3c6f76b49d673dd5c4c7984572c808fe
root <root@srv1433861.hstgr.cloud>
2026-03-27 16:18:21 +0000

    Fixed formatting in web frontend files by adding proper newlines

--- lib/perl/OpenLash/WebUI.pm
+++ lib/perl/OpenLash/WebUI.pm
@@ -1,1 +1,23 @@
-package OpenLash::WebUI; use strict; use warnings; use Template; sub new { my () = @_; my  = bless { template => Template->new({ INCLUDE_PATH => "/root/OpenLash/webserver/tpl", }), }, ; return ; } sub render { my (, , ) = @_; my ; ->{template}->process(, , $output) or die ->{template}->error(); return ; } 1;
+package OpenLash::WebUI;
+use strict;
+use warnings;
+use Template;
+
+sub new {
+    my () = @_;
+    my  = bless {
+        template => Template->new({
+            INCLUDE_PATH => /root/OpenLash/webserver/tpl,
+        }),
+    }, ;
+    return ;
+}
+
+sub render {
+    my (, , ) = @_;
+    my ;
+    ->{template}->process(, , $output) or die ->{template}->error();
+    return ;
+}
+
+1;
